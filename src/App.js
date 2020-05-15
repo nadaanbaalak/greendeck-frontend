a97da8784e8faeccd6e12d6d214b8c6adad8f175
nadaanbaalak <abhi.sharma2800015@gmail.com>
2020-05-15 15:04:43 +0530

    Removed proxy from .json

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Display from "./components/productDisplay";
+import Navbar from "./components/navbar";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Display />
+    </>
+  );
+}
+
+export default App;
